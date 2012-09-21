d7b6ec29b0225f0020de564d0763b105568d9e6b
Ben Roberts <ben@sihnon.net>
2012-09-22 00:02:41 +0100

    Prepopulate the generator form with test data for debugging purposes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,7 @@
 function testplanGenerator($scope, $http) {
     
+    $scope.dev = true;
+    
     $scope.commands = {};
     
     $scope.templates = [];
@@ -35,9 +37,25 @@ function testplanGenerator($scope, $http) {
         $scope.templates = data;
         if ($scope.templates.length > 0) {
             $scope.selectedTemplate = $scope.templates[0];
+            
+            if ($scope.dev) {
+                $scope.reference = "GZ:C123456";
+                $scope.selectedExecutionType = 2;
+                
+                $scope.addTemplate($scope.templates[0]);
+                
+                $scope.testplans[0].newDeviceType = $scope.templates[0].deviceTypes[0];
+                $scope.testplans[0].newHostname = "SwitchA";
+                $scope.addDevice($scope.testplans[0]);
+                
+                $scope.testplans[0].devices[0].commands[1].newListItemValue = "GigabitEthernet1/1";
+                $scope.testplans[0].devices[0].commands[3].newListItemValue = 123;
+                $scope.addListItem($scope.testplans[0].devices[0].commands[1]);
+                $scope.addListItem($scope.testplans[0].devices[0].commands[3]);
+            }
         }
     });
-
+    
     $scope.displayCommand = function(command, parameters) {
         var output = '<div class="code">';
         output += $scope.generateCommand(command, parameters);
