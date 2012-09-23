f44e6b6ceae5d938bf7ce17ac4aebdefc5ad7c20
Ben Roberts <ben@sihnon.net>
2012-09-23 18:43:15 +0100

    add timeago directive

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,2 +1,11 @@
-angular.module('tpeDirectives', []);
-
+angular.module('tpeDirectives', []).directive('ngTimeago', function(dateFilter) {
+    return function(scope, element, attrs) {
+        scope.$watch(attrs.ngModel, function(value) {
+            var time = 'unknown';
+            if (value) {
+                time = dateFilter(value * 1000, 'yyyy-MM-ddTHH:mm:ssZ');
+            }
+            element.attr('datetime', time).attr('title', time).text(time);
+        });
+    }
+});
