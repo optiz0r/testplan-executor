6647001c3a3c3fcff94c22214a23854dde7ee89f
Ben Roberts <ben@sihnon.net>
2012-10-07 22:02:39 +0100

    make CiscoTelnet a proper plugin (fix copy/paste errors)

--- source/lib/TPE/DeviceHandler/Plugin/CiscoTelnet.class.php
+++ source/lib/TPE/DeviceHandler/Plugin/CiscoTelnet.class.php
@@ -1,6 +1,6 @@
 <?php
 
-class RippingCluster_Source_Plugin_MkvInfo extends RippingCluster_PluginBase implements RippingCluster_Source_IPlugin {
+class TPE_DeviceHandler_Plugin_CiscoTelnet extends TPE_PluginBase implements TPE_DeviceHandler_IPlugin {
     
     /**
      * Name of this plugin
@@ -8,8 +8,12 @@ class RippingCluster_Source_Plugin_MkvInfo extends RippingCluster_PluginBase imp
      */
     const PLUGIN_NAME = 'CiscoTelnet';
     
-    public function init($options) {
+    public static function init() {
         
+    }
+    
+    public static function create($options) {
+        return new self($options);
     }
     
     public function execute() {
